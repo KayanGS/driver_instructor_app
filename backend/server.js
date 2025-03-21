94daf3e0f5877ce33ba4e3cd017a82a4f849b5a8
Kayan Gemingani <119429909+KayanGS@users.noreply.github.com>
2025-03-21 16:05:07 +0000

    initialize React frontend
    - Created frontend/ directory using Create React App

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,24 @@
 
 const express = require('express'); // Import express
 const connectDB = require('./config/database.js'); // Import database connection
+
+connectDB(); // Connect to database
+
+//Middleware
+const app = express(); // Initialize express
+app.use(express.json()); // Parse JSON bodies
+
+// Define routes
 const userRoutes = require('./routes/user_routes'); // Import user routes 
 const lessonRoutes = require('./routes/lesson_routes');// Import lesson routes
 const purchaseRoutes = require('./routes/purchase_routes'); // Import purchase routes
 const timeSlotRoutes = require('./routes/timeSlot_routes'); // Import timeSlot routes
 
-connectDB(); // Connect to database
-
-const app = express(); // Initialize express
-
-// Middleware
-app.use(express.json()); // Parse JSON bodies
 app.use('/api', userRoutes); // Use user routes
 app.use('/api', lessonRoutes); // Use lesson routes
 app.use('/api', purchaseRoutes); // Use purchase routes
 app.use('/api', timeSlotRoutes); // Use timeSlot routes
+
 
 // Start server
 const PORT = process.env.PORT || 5000; // Set port
