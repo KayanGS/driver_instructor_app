6eca14833ab724aa0a0ad7123756b7fe2c68d090
Kayan Gemingani <119429909+KayanGS@users.noreply.github.com>
2025-05-06 11:31:26 +0100

    Update WelcomePage.js

--- frontend/src/pages/WelcomePage.js
+++ frontend/src/pages/WelcomePage.js
@@ -19,9 +19,7 @@ const WelcomePage = () => {
         if (!userId) return;
 
         try {
-            const api = process.env.NODE_ENV === 'development'
-                ? 'http://localhost:5000/api'
-                : 'https://driver-instructor-app-backend.onrender.com/api';
+            const api = 'https://driver-instructor-app-backend.onrender.com/api';
 
             const res = await fetch(`${api}/users/${userId}`, {
                 method: 'GET',
